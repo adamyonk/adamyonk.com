2ea585dc4106c4542d2b6a6afdb76353f1e3a628
Adam Jahnke <adamyonk@icloud.com>
2023-02-26 09:58:21 -0600

    Next/font codemod

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import "styles/index.css";
-import { JetBrains_Mono } from "@next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 import Footer from "components/footer";
 
 const jetbrains_mono = JetBrains_Mono({
