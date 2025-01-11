89be5b8f104a92a857b14046b1949e96cb25bbe6
Adam Jahnke <adamyonk@icloud.com>
2025-01-11 16:42:22 -0600

    Tweak style

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    turbo: {
+      treeShaking: true
+    }
+  }
 }
- 
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 })
- 
+
 module.exports = withBundleAnalyzer(nextConfig)
